53862a592161793616ddbf4bf30fc6d5d6edbc7c
Anwar Lopez <anwarlo247@gmail.com>
2019-05-01 20:59:50 -0500

    added controller exercise files

--- src/main/java/com/codeup/springblog/SpringBlogApplication.java
+++ src/main/java/com/codeup/springblog/SpringBlogApplication.java
@@ -1,6 +1,6 @@
 package com.codeup.springblog;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication

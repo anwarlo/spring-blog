b4f5ec9a9f973f5406fb3c20b2363572a53a458a
Anwar Lopez <anwarlo247@gmail.com>
2019-05-03 17:41:17 -0500

    added roll dice controller

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,4 +1,30 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class RollDiceController {
+    @GetMapping("/roll-dice/{num}")
+    public String rollDice(@PathVariable int num, Model vmodel){
+
+        int random = (int)(Math.random()*6+1);
+
+        String result;
+
+        if (random == num){
+            result = "win";
+        }else{
+            result = "lost!";
+        }
+
+        vmodel.addAttribute("RandomNumber",random);
+        vmodel.addAttribute("Result",result);
+
+        //Sends to html
+        return "dice";
+
+    }
 }
